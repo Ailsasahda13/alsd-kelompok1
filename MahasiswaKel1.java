795fbcdaae7fcae06db255ff00e3102c011e9dd4
Ailsasahda13 <ailsashda13@gmail.com>
2025-04-21 15:06:41 +0700

    Ailsa

---
+++ MahasiswaKel1.java
@@ -0,0 +1,12 @@
+public class MahasiswaKel1 {
+
+    String NIM;
+    String nama;
+    String programStudi;
+
+    MahasiswaKel1(String nim, String name, String prodi) {
+        NIM = nim;
+        nama = name;
+        programStudi = prodi;
+    }
+}
---
+++ MataKuliahKel1.java
@@ -0,0 +1,12 @@
+public class MataKuliahKel1 {
+    
+    String kodeMK;
+    String namaMK;
+    int sks;
+
+    MataKuliahKel1(String kodeMataKuliah, String namaMataKuliah, int sks) {
+        kodeMK = kodeMataKuliah;
+        namaMK = namaMataKuliah;
+        sks = sks;
+    }
+}
